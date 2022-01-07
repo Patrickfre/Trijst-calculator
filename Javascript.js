13c5a1977bb0b4239091b6788bae108a8efa5976
Patriks_f <Patriks.aabele@gmail.com>
2022-01-07 15:12:02 +0200

    Calc
    trijst laikums

---
+++ Javascript.js
@@ -0,0 +1,26 @@
+
+function nolasa() {
+    const malas = document.getElementsByClassName("mala");
+    const m1 = parseFloat(mala1.value);
+    const m2 = parseFloat(mala2.value);
+    const m3 = parseFloat(mala3.value);
+    if (m1 > 0 && m2 > 0 && m3 > 0) {
+        console.log({ m1, m2, m3 });
+        return { m1, m2, m3 };
+    } else {
+        console.log(false);
+        return false;
+    }
+}
+function perimeter(m1, m2, m3); {
+    const p = m1 + m2 + m3;
+    console.log(p);
+    return p;
+}
+function laukums(m1, m2, m3); {
+    const pusper = perimeter(m1, m2, m3) / 2;
+    let tlaukums = Math.sqrt(
+    pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3));
+    console.log(tlaukums);
+    return tlaukums;
+}
