7bcf233cd2d9b0c5235263e8e7daf5a82e507116
Patrickfre <Patriks.freiman@gmail.com>
2022-01-10 09:36:28 +0200

    Updata
    kautkas

--- Javascript.js
+++ Javascript.js
@@ -12,15 +12,18 @@ function nolasa() {
         return false;
     }
 }
-function perimeter(m1, m2, m3); {
+function perimeter(m1, m2, m3) {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
     const p = m1 + m2 + m3;
     console.log(p);
     return p;
 }
 function laukums(m1, m2, m3); {
-    const pusper = perimeter(m1, m2, m3) / 2;
-    let tlaukums = Math.sqrt(
-    pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3));
-    console.log(tlaukums);
-    return tlaukums;
+    const pusper = (m1 + m2 + m3) / 2;
+
+    let laukums = Math.sqrt(pusper * (pusper - m1) * (pusper - m2) * (pusper - m3));
+    console.log(laukums);
+    return laukums;
 }
