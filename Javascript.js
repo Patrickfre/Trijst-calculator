3893339998dd65764173a025507306c10f0ce23f
Patrickfre <Patriks.freiman@gmail.com>
2022-01-14 13:53:03 +0200

    Trying to fix
    failing badly

--- Javascript.js
+++ Javascript.js
@@ -12,17 +12,16 @@ function nolasa() {
         return false;
     }
 }
-function perimeter(m1, m2, m3) {
+function perimetrs(m1, m2, m3) {
     const m = nolasa();
     m1 = m.m1; m2 = m.m2; m3 = m.m3;
     console.log(m1, m2, m3);
-    const p = m1 + m2 + m3;
-    console.log(p);
-    return p;
+    const perimetrs = m1 + m2 + m3;
+    console.log(perimetrs);
+    return perimetrs;
 }
 function laukums(m1, m2, m3); {
     const pusper = (m1 + m2 + m3) / 2;
-
     let laukums = Math.sqrt(pusper * (pusper - m1) * (pusper - m2) * (pusper - m3));
     console.log(laukums);
     return laukums;
