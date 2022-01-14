807fd7f95177ea6b3c1ffeea93fbc82fd279a8d5
Patrickfre <Patriks.freiman@gmail.com>
2022-01-14 14:15:52 +0200

    Fix
    Very fix good

--- script.js
+++ script.js
@@ -16,13 +16,23 @@ function perimetrs(m1, m2, m3) {
     const m = nolasa();
     m1 = m.m1; m2 = m.m2; m3 = m.m3;
     console.log(m1, m2, m3);
-    const perimetrs = m1 + m2 + m3;
-    console.log(perimetrs);
-    return perimetrs;
+    const p = m1 + m2 + m3;
+    console.log(p);
+    return p;
 }
-function laukums(m1, m2, m3); {
-    const pusper = (m1 + m2 + m3) / 2;
+function laukums(m1, m2, m3) {
+    const m=nolasa();
+    m1=m.m1; m2=m.m2; m3=m.m3;
+    const pusper = perimetrs(m1,m2,m3) / 2;
     let laukums = Math.sqrt(pusper * (pusper - m1) * (pusper - m2) * (pusper - m3));
     console.log(laukums);
     return laukums;
+}
+function rezultats(){
+    const m=nolasa();
+    m1=m.m1; m2=m.m2; m3=m.m3;
+    console.log(m1,m2,m3);
+    if(!nolasa()){
+        t="Nepareizi ievaddati, trijstūra malu vērtībām jābūt lielākām par 0";
+    }
 }
